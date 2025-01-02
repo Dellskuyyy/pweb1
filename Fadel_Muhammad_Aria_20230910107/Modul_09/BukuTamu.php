e58bdbbc7c46666b14e45841bddee6b2ffb6ba32
Dellskuyyy <20230910107@uniku.ac.id>
2025-01-02 23:24:58 +0700

    Inittial commit : Menambahkan File Modul 9

---
+++ Fadel_Muhammad_Aria_20230910107/Modul_09/BukuTamu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guestbook</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container my-4">
+        <h2>Buku Tamu</h2>
+        <form action="SubmitBukuTamu.php" method="POST">
+            <div class="mb-3">
+                <label for="name" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Pesan</label>
+                <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Kirim</button>
+        </form>
+    </div>
+</body>
+</html>
